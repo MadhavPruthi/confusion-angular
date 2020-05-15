f4fe9375110497633ce00641b34ac556d13ecf3b
Madhav Pruthi <madhavpruthi.becse16@pec.edu.in>
2020-05-15 20:09:35 +0530

    dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MatListModule } from "@angular/material/list";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
+
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
@@ -20,8 +22,10 @@ import { DishDetailComponent } from "./dish-detail/dish-detail.component";
 
 import { DishService } from "./services/dish.service";
 import { PromotionService } from "./services/promotion.service";
+import { LeaderService } from "./services/leader.service";
 
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
   declarations: [
@@ -33,6 +37,7 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
     HomeComponent,
     AboutComponent,
     ContactComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -44,8 +49,10 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
+    MatDialogModule,
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent],
+  entryComponents: [LoginComponent],
 })
 export class AppModule {}
